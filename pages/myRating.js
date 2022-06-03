4bcd5fe3842a531363a4f5f3d730267a023d2ec3
ejiro-ghene <stephanieukiwe@gmail.com>
2022-06-03 22:08:38 +0100

    touchups

--- pages/myRating.js
+++ pages/myRating.js
@@ -18,36 +18,6 @@ for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
     i = this.getAttribute("data-rate");
 
-    showValue.innerText = " " + i + " " + "out of 5";
+    showValue.innerText = "You selected" + " " + i + " " + "out of 5";
   });
 }
-// buttons.forEach((element) => {});
-// let ratings = document.querySelector(".current_rating");
-
-// buttons.forEach((rate, idx) => {
-//   rate.addEventListener;
-// });
-
-// for (let i = 0; i < buttons.length; i++) {
-//   buttons[i].buttonValue = i + 1;
-//   ["mouseover", "mouseout", "click"].forEach(function (e) {
-//     buttons[i].addEventListener(e, buttonRate);
-//   });
-// }
-
-// function buttonRate(e) {
-//   let type = e.type;
-//   let buttonValue = this.buttonValue;
-//   if (type === "click") {
-//     if (buttonValue > 1) {
-//       ratings.innerHTML = "you rated this" + buttonValue + "dsjhnb";
-//     }
-//   }
-//   statusbar.forEach(function (ele, ind) {
-//     // if (type == "click") {
-//     //   if (ind < buttonValue) {
-//     //     ele.classList.add;
-//     //   }
-//     // }
-//   });
-// }
